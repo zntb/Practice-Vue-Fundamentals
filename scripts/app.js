0e5bc125dd9ea2d40a669da32b3764f3ee2ea582
zntb <zenetibi@yahoo.com>
2023-10-23 11:10:22 +0300

    Outputting lists of data

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,19 @@
 const TodosApp = {
   data() {
     return {
-      newTodo: "Learn Vue.js!",
+      todos: [],
       enteredTodoText: "",
     };
   },
   methods: {
     saveTodo(event) {
       event.preventDefault();
-      this.newTodo = this.enteredTodoText;
+      const newTodo = {
+        text: this.enteredTodoText,
+        id: new Date().toISOString(),
+      };
+      this.todos.push(newTodo);
+
       this.enteredTodoText = "";
     },
   },
