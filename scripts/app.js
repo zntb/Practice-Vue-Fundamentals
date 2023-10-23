3902bc73da333c1c75745d2e985da273b0631ed9
zntb <zenetibi@yahoo.com>
2023-10-23 12:51:30 +0300

    Sending a POST Http request

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,7 @@ const TodosApp = {
     };
   },
   methods: {
-    saveTodo(event) {
+    async saveTodo(event) {
       event.preventDefault();
 
       if (this.editedTodoId) {
@@ -26,10 +26,35 @@ const TodosApp = {
         this.editedTodoId = null;
       } else {
         // Creating...
+
+        let response;
+
+        try {
+          response = await fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: JSON.stringify({
+              text: this.enteredTodoText,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        const responseData = await response.json();
         const newTodo = {
           text: this.enteredTodoText,
-          id: new Date().toISOString(),
+          id: responseData.createdTodo.id,
         };
+
         this.todos.push(newTodo);
       }
 
