21369ed35212c8ca1f35bc7a6f04797a47a08e96
zntb <zenetibi@yahoo.com>
2023-10-23 12:08:50 +0300

    Updating data

--- scripts/app.js
+++ scripts/app.js
@@ -3,18 +3,44 @@ const TodosApp = {
     return {
       todos: [],
       enteredTodoText: "",
+      editedTodoId: null,
     };
   },
   methods: {
     saveTodo(event) {
       event.preventDefault();
-      const newTodo = {
-        text: this.enteredTodoText,
-        id: new Date().toISOString(),
-      };
-      this.todos.push(newTodo);
+
+      if (this.editedTodoId) {
+        // Updating...
+        const todoId = this.editedTodoId;
+        const todoIndex = this.todos.findIndex(function (todoItem) {
+          return todoItem.id === todoId;
+        });
+
+        const updatedTodoItem = {
+          id: this.todos[todoIndex].id,
+          text: this.enteredTodoText,
+        };
+
+        this.todos[todoIndex] = updatedTodoItem;
+        this.editedTodoId = null;
+      } else {
+        // Creating...
+        const newTodo = {
+          text: this.enteredTodoText,
+          id: new Date().toISOString(),
+        };
+        this.todos.push(newTodo);
+      }
 
       this.enteredTodoText = "";
+    },
+    startEditTodo(todoId) {
+      this.editedTodoId = todoId;
+      const todo = this.todos.find(function (todoItem) {
+        return todoItem.id === todoId;
+      });
+      this.enteredTodoText = todo.text;
     },
   },
 };
